0eef81a8cae691d1ed460eed3d50823d3e8ba459
MetuzaelAmek <80079170+MetuzaelAmek@users.noreply.github.com>
2021-04-09 16:40:53 -0300

    Adicionando o Projeto CRUD

---
+++ cadpessoas_metuzael/src/main/java/br/com/metuzael/cadpessoas/CadpessoasMetuzaelApplication.java
@@ -0,0 +1,13 @@
+package br.com.metuzael.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasMetuzaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasMetuzaelApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_metuzael/src/test/java/br/com/metuzael/cadpessoas/CadpessoasMetuzaelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.metuzael.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasMetuzaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
