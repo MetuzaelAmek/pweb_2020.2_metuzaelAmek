4fe773253838503196ae7a132e8e02315e9cd3b8
MetuzaelAmek <80079170+MetuzaelAmek@users.noreply.github.com>
2021-04-09 18:25:09 -0300

    Editando PessoaController

--- cadpessoas_metuzael/src/main/java/br/com/metuzael/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_metuzael/src/main/java/br/com/metuzael/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,21 @@
 package br.com.metuzael.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.metuzael.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepositorio;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
 }
