131d4926e49eca3e22e2278aad166dac873132ec
MetuzaelAmek <80079170+MetuzaelAmek@users.noreply.github.com>
2021-04-09 18:10:52 -0300

    Adicionando o MVC no projeto

---
+++ cadpessoas_metuzael/src/main/java/br/com/metuzael/cadpessoas/model/Pessoa.java
@@ -0,0 +1,71 @@
+package br.com.metuzael.cadpessoas.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor 
+@Data
+@Entity
+@Table(name="pessoas")
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 9168469812928720882L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idPessoa;
+	private String nome;
+	private String email;
+	private String genero;
+	
+	@Column(nullable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private LocalDate DTnascimento;
+	public Long getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public LocalDate getDTnascimento() {
+		return DTnascimento;
+	}
+	public void setDTnascimento(LocalDate dTnascimento) {
+		DTnascimento = dTnascimento;
+	}
+	
+	
+	
+}
